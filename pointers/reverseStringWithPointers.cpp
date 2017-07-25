24def557e39a3cb84e08a476408f40296ab57290
Ola Karlsson <ola.karlsson@bahnhof.se>
2017-07-25 23:53:53 +0200

    First draft
    More fiddling with pointers

---
+++ pointers/reverseStringWithPointers.cpp
@@ -0,0 +1,30 @@
+//============================================================================
+// Name        : Reverse.cpp
+// Author      : Ola Karlsson
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Reversing a string with pointers in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	char text[] = "hello";
+
+	int nChar = sizeof(text) - 1;
+
+	char *pStart = text;
+
+	char *pEnd = text + nChar - 1;
+
+	//cout << *pEnd << endl;
+
+	while (pEnd >= pStart) {
+		cout << *pEnd << flush;
+		pEnd--;
+	}
+
+	return 0;
+}
